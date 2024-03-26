28b1151e4ae7ff2dbfa7aa1813909aec663aeb2c
Cameron <you@example.com>
2024-03-26 13:29:26 -0300

    Calendar

---
+++ Project/app/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'; // Import createApp from vue
+
+import App from './App.vue';
+
+// Create the Vue app using createApp
+const app = createApp(App);
+
+// Mount the app to the element with id 'app'
+app.mount('#app');
