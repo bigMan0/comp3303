564e819899b6c320155619358aa6ccfa7a51b53b
kylie <133984183+kylidevi@users.noreply.github.com>
2024-03-26 13:43:54 -0300

    added the server and model js files

---
+++ Project/app/models/calendarModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const contactSchema = mongoose.Schema(
+    { // the _id will do it itself in mongodb
+        id: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: [true, "Please enter a event title"]
+        },
+        date: {
+            type: Date,
+            required: [true, "Please enter a date"]
+        },
+        type: {
+            type: String,
+            required: true,
+            default: "NULL"
+        },
+    }
+)
+
+// Node-Api is the database, My_contacts is the file
+const Calendar = mongoose.model('calendar', contactSchema, 'cal');
+module.exports = Calendar;
