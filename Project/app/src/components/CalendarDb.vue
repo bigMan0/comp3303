e64dd2454a851552b19573f912eea0409fb209c8
Your Name <you@example.com>
2024-03-28 19:59:59 -0300

    box edits

--- Project/app/src/components/CalendarDb.vue
+++ Project/app/src/components/CalendarDb.vue
@@ -1,39 +1,38 @@
 <template>
-    <div>
-      <div v-if="posts && posts.length">
-        <div v-for="post of posts" :key="post._id">
-          <strong>{{ post.title }} </strong>  
-          <!-- {{ post._id }} -->
-        </div>
+  <div>
+    <div v-if="posts && posts.length">
+      <div v-for="post of posts" :key="post._id">
+        <strong>{{ post.title }} </strong>  
+        <!-- {{ post._id }} -->
       </div>
-      <ul v-if="errors && errors.length">
-        <li v-for="error of errors" :key="error.id">
-          {{error.message}}
-        </li>
-      </ul>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        posts: [],
-        errors: []
-      }
-    },
-  
-    // Fetches posts when the component is created.
-    async created() {
-      try {
-        const response = await axios.get('http://localhost:3001/calendar')
-        this.posts = response.data
-      } catch (e) {
-        this.errors.push(e)
-      }
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors" :key="error.id">
+        {{error.message}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:3001/calendar')
+      this.posts = response.data
+    } catch (e) {
+      this.errors.push(e)
     }
   }
-  </script>
-  
+}
+</script>
--- Project/app/src/components/CalendarMonthDayItem.vue
+++ Project/app/src/components/CalendarMonthDayItem.vue
@@ -1,79 +1,94 @@
 <template>
-    <li
-      class="calendar-day"
-      :class="{
-        'calendar-day--not-current': !day.isCurrentMonth,
-        'calendar-day--today': isToday
-      }"
-    >
-      <span>{{ label }}</span>
-    </li>
-  </template>
-  
-  <script>
-  import dayjs from "dayjs";
-  
-  export default {
-    name: "CalendarMonthDayItem",
-  
-    props: {
-      day: {
-        type: Object,
-        required: true
-      },
-  
-      isCurrentMonth: {
-        type: Boolean,
-        default: false
-      },
-  
-      isToday: {
-        type: Boolean,
-        default: false
-      }
+  <li
+    class="calendar-day"
+    :class="{
+      'calendar-day--not-current': !day.isCurrentMonth,
+      'calendar-day--today': isToday
+    }"
+  >
+    <span>{{ label }}</span>
+    <div v-if="label === '9' && day.isCurrentMonth" class="event-box">
+      <div class="event-title">hello</div>
+    </div>
+  </li>
+</template>
+
+<script>
+import dayjs from "dayjs";
+
+export default {
+  name: "CalendarMonthDayItem",
+
+  props: {
+    day: {
+      type: Object,
+      required: true
     },
-  
-    computed: {
-      label() {
-        return dayjs(this.day.date).format("D");
-      }
+
+    isCurrentMonth: {
+      type: Boolean,
+      default: false
+    },
+
+    isToday: {
+      type: Boolean,
+      default: false
     }
-  };
-  </script>
-  
-  <style scoped>
-  .calendar-day {
-    position: relative;
-    min-height: 100px;
-    font-size: 16px;
-    background-color: #fff;
-    color: var(--grey-800);
-    padding: 5px;
+  },
+
+  computed: {
+    label() {
+      return dayjs(this.day.date).format("D");
+    }
   }
-  
-  .calendar-day > span {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    right: 2px;
-    width: var(--day-label-size);
-    height: var(--day-label-size);
-  }
-  
-  .calendar-day--not-current {
-    background-color: var(--grey-100);
-    color: var(--grey-300);
-  }
-  
-  .calendar-day--today {
-    padding-top: 4px;
-  }
-  
-  .calendar-day--today > span {
-    color: #fff;
-    border-radius: 9999px;
-    background-color: var(--grey-800);
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.calendar-day {
+  position: relative;
+  min-height: 100px;
+  font-size: 16px;
+  background-color: #fff;
+  color: var(--grey-800);
+  padding: 5px;
+}
+
+.calendar-day > span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 2px;
+  width: var(--day-label-size);
+  height: var(--day-label-size);
+}
+.event-box {
+  position: absolute;
+  top: 25px;
+  left: 15px;
+  padding: 10px;
+  width: calc(80% - 10px);
+  height: calc(80% - 10px);
+  border: 2px solid #000;
+}
+
+.event-title {
+  font-weight: bold;
+  padding: 5px;
+}
+.calendar-day--not-current {
+  background-color: var(--grey-100);
+  color: var(--grey-300);
+}
+
+.calendar-day--today {
+  padding-top: 4px;
+}
+
+.calendar-day--today > span {
+  color: #fff;
+  border-radius: 9999px;
+  background-color: var(--grey-800);
+}
+</style>
